91aab5b846bb4f98234d2c9a75bc9991b3eaf1da
ALEXIS RAJIS <arajis06@gmail.com>
2019-08-12 18:52:46 -0400

    working app

--- models/product.js
+++ models/product.js
@@ -1,41 +1,41 @@
 module.exports = function(sequelize, DataTypes) {
-    var Product = sequelize.define("Product", {
-        productName: {
-          type: DataTypes.STRING, 
-          allowNull: false, 
-        }, 
-        
-        store: {
-          type: DataTypes.STRING, 
-          allowNull: false, 
-        },
-        
-        price: {
-          type: DataTypes.INTEGER, 
-          allowNull: false, 
-        },
+  var Product = sequelize.define("Product", {
+      productName: {
+        type: DataTypes.STRING, 
+        allowNull: false, 
+      }, 
+      
+      store: {
+        type: DataTypes.STRING, 
+        allowNull: false, 
+      },
+      
+      price: {
+        type: DataTypes.INTEGER, 
+        allowNull: false, 
+      },
 
-        isPurchased: {
-            type: DataTypes.BOOLEAN, 
-            defaultValue: false, 
-        }, 
+      isPurchased: {
+          type: DataTypes.BOOLEAN, 
+          defaultValue: false, 
+      }, 
 
-        // userId: {
-        //   type: DataTypes.INTEGER, 
-        //   defaultValue: 0, 
-        // }
+      // userId: {
+      //   type: DataTypes.INTEGER, 
+      //   defaultValue: 0, 
+      // }
 
-      });
+    });
 
-      // Product.associate = function(models) {
-      //   // We're saying that a Post should belong to an Author
-      //   // A Post can't be created without an Author due to the foreign key constraint
-      //   Product.belongsTo(models.User, {
-      //     foreignKey: {
-      //       allowNull: false
-      //     }
-      //   });
-      // };
+    // Product.associate = function(models) {
+    //   // We're saying that a Post should belong to an Author
+    //   // A Post can't be created without an Author due to the foreign key constraint
+    //   Product.belongsTo(models.User, {
+    //     foreignKey: {
+    //       allowNull: false
+    //     }
+    //   });
+    // };
 
-      return Product;
-    };
+    return Product;
+  };
