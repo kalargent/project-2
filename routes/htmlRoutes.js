b412139c02db9189b5bb7a9329767e0201b88e9b
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-08-12 19:26:43 -0400

    committing before pulling master

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,8 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
-
+  app.get("/", function(req, res) {
     /**
     1. Check if there is query param
     
@@ -22,80 +21,46 @@ module.exports = function (app) {
           id: req.query.user
         },
         include: [db.Product]
-      }).then(function (dbUser) {
+      }).then(function(dbUser) {
         console.log(dbUser);
         // res.send(dbUser);
-       res.render("userRegistry",
-       dbUser)
+        res.render("userRegistry", dbUser);
       });
     } else {
-res.render("index");
+      res.render("index");
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // db.Example.findAll({}).then(function (dbExamples) {
-    //   res.render("index", {
-    //     msg: "Welcome!",
-    //     examples: dbExamples
-    //   });
-    // });
   });
-  
 
-  // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
+  app.get("/registry/:id", function(req, res) {
+    console.log(parseInt(req.params.id)); 
+    if (parseInt(req.params.id)) {
+      db.Product.findAll({
+        where: {
+          userId: parseInt(req.params.id)
+        },
+        include: [db.Products]
+      }).then(function(products) {
+        console.log(products);
+        res.render("registry", products);
+      });
+    } else {
+      res.render("index");
+    }
+  });
+
+  // app.get("/registry", function (req, res) {
+  //   db.Product.findAll({
+
+  //   }).then(function (dbProduct) {
+  //     var productObj = {
+  //       Products: dbProduct
+  //     }
+  //     res.render("registry", productObj);
   //   });
   // });
 
-  app.get("/registry", function (req, res) {
-    db.Product.findAll({}).then(function (dbProduct) {
-      var productObj = {
-        Products: dbProduct
-      }
-      res.render("registry", productObj);
-    });
-  });
-
-  // app.get("/:user", function (req, res){
-  //   db.users.findOne({
-  //     where: {
-  //       firstName: req.body
-  //     }, 
-  //     include: [db.Product]
-  //   }).then (function (dbUser) {
-  //     // res.json(dbUser); 
-  //     res.render("userRegistry"); 
-  //     console.log("you returned a user"); 
-  //     console.log(dbUser); 
-  //   })
-  // })
-
-  app.get("/", function (req, res) {
-    if (req.query) {
-      res.render("userRegistry")
-    }
-  })
-
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
